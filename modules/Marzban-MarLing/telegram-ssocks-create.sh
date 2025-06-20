89102fa8ad1acf4b57780e670826237040d73a34
GawrAme <97426017+GawrAme@users.noreply.github.com>
2025-06-20 19:00:31 +0700

    Update telegram-ssocks-create.sh

--- modules/Marzban-MarLing/telegram-ssocks-create.sh
+++ modules/Marzban-MarLing/telegram-ssocks-create.sh
@@ -6,10 +6,12 @@ EXPIRED="$3"
 
 tunnel_name="Shadowsocks"
 tunnel_type="SHADOWSOCKS"
-limit_gb="200"
+limit_gb="1024"
 limit_bytes=$((limit_gb * 1024 * 1024 * 1024))
 expired_timestamp=$(date -d "+${EXPIRED} days" +%s)
+current_date=$(date "+%Y-%m-%d %H:%M:%S")
 
+DOMAIN=$(cat /root/domain)
 api_host="127.0.0.1"
 api_port="YOUR_API_PORT"
 api_username="YOUR_API_USERNAME"
@@ -30,7 +32,7 @@ req_json='{
   "data_limit_reset_strategy": "month",
   "expire": '"${expired_timestamp}"',
   "inbounds": {
-    "vmess": [
+    "shadowsocks": [
       "'"${tunnel_type}"'_OUTLINE"
     ]
   },
@@ -40,10 +42,11 @@ req_json='{
     "expire": 0,
     "fire_on_either": true
   },
-  "note": "",
+  "note": "CREATED AT '"${current_date}"'",
   "proxies": {
-    "vmess": {
-      "id": "'"${PASSWORD}"'"
+    "shadowsocks": {
+      "password": "'"${PASSWORD}"'",
+      "method": "aes-128-gcm"
     }
   },
   "status": "active",
@@ -74,4 +77,26 @@ echo -e "Password: <code>${PASSWORD}</code>"
 echo -e "Expired: <code>$(date -d "@${expire}" '+%Y-%m-%d %H:%M:%S')</code>"
 echo -e "Data Limit: <code>${limit_gb}</code> GB"
 echo -e "Link : <code>${link}</code>"
+echo -e "<b>+++++ End of Account Details +++++</b>"
+
+echo -e "=======-XRAY/${tunnel_name}-OUTLINE======="
+echo -e ""
+echo -e "Remarks: ${USERNAME}"
+echo -e "Domain: ${DOMAIN}"
+echo -e "Quota: ${limit_gb}GB"
+echo -e "Reset Quota Strategy: Bulanan"
+echo -e "================================="
+echo -e "Port Outline: 1080"
+echo -e "================================="
+echo -e "password: ${PASSWORD}"
+echo -e "Method: aes-128-gcm"
+echo -e "network: none"
+echo -e "================================="
+echo -e "alpn: h2, http/1.1"
+echo -e "tls: none"
+echo -e "allowInsecure: true"
+echo -e "================================="
+echo -e "Link Subscription : https://${DOMAIN}${SUBS}"
+echo -e "================================="
+echo -e "Masa Aktif: $(date -d "@${expire}" '+%Y-%m-%d %H:%M:%S')"
 echo -e "<b>+++++ End of Account Details +++++</b>"
