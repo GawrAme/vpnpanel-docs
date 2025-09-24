dff4791e23f13d5f16970d3b6f7654c74f384788
GawrAme <97426017+GawrAme@users.noreply.github.com>
2025-09-24 18:58:35 +0700

    Update telegram-vless-trial.sh

--- modules/Marzban-MarLing/telegram-vless-trial.sh
+++ modules/Marzban-MarLing/telegram-vless-trial.sh
@@ -86,12 +86,6 @@ req_json='{
   "inbounds": {
     "vless": '"${inbounds_json}"'
   },
-  "next_plan": {
-    "add_remaining_traffic": false,
-    "data_limit": 0,
-    "expire": 0,
-    "fire_on_either": true
-  },
   "note": "CREATED AT '"${current_date}"'",
   "proxies": {
     "vless": {
